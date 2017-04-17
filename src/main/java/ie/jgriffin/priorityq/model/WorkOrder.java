64009aa977e9ca5677f2b6c7b505932661fc88ec
John Griffin <griffin.john.michael@gmail.com>
2017-04-17 22:39:21 +0100

    add basic spring boot files

---
+++ src/main/java/ie/jgriffin/priorityq/model/WorkOrder.java
@@ -0,0 +1,17 @@
+package ie.jgriffin.priorityq.model;
+
+/**
+ * Created by jgriffin on 17/04/2017.
+ */
+public class WorkOrder {
+
+    private final Double id;
+
+    public WorkOrder(Double id) {
+        this.id = id;
+    }
+
+    public Double getId() {
+        return id;
+    }
+}
---
+++ src/main/java/ie/jgriffin/priorityq/rest/RestRequestController.java
@@ -0,0 +1,10 @@
+package ie.jgriffin.priorityq.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jgriffin on 17/04/2017.
+ */
+@RestController
+public class RestRequestController {
+}
